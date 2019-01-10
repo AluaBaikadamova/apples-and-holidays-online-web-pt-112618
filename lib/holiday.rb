616d31f68330749c7e545289915663054c662249
Alua Baikadamova <abaikad00@gmail.com>
2019-01-10 08:50:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -104,7 +104,7 @@ s2.gsub!("_", " ")
 s2.capitalize
 s2.split.map { |x| x.capitalize }.join(" ")
 #s2.gsub(/\S+/, &:capitalize)
-  puts "  #{s2}: #{s.chomp(", ")}"
+  puts "  #{s2.capitalize}: #{s.chomp(", ")}"
     end
   end
 
