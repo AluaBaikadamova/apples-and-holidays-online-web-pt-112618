0ec109e497d4c5d646d264f3c189980834e1f821
Alua Baikadamova <abaikad00@gmail.com>
2019-01-10 07:50:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,10 +58,10 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   details<<holiday_name
     end
   end
-end
+
   
 
-  #holiday_hash[:season]<<{:holiday_name=>:supply_array}
+  # holiday_hash[:season]<<{:holiday_name=>:supply_array}
   
 return holiday_hash
 end
