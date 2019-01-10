66acc5cfee21723d81900feb09749dfab5bb66cc
Alua Baikadamova <abaikad00@gmail.com>
2019-01-10 08:50:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -101,6 +101,7 @@ def all_supplies_in_holidays(holiday_hash)
   end
   s2=attribute.to_s
 s2.gsub!("_", " ")
+s2.capitalize
 s2.split.map { |x| x.capitalize }.join(" ")
 #s2.gsub(/\S+/, &:capitalize)
   puts "  #{s2}: #{s.chomp(", ")}"
